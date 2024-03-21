08337bb1d796e917bcb4e79d5aa3120fb30e2052
ErikWe <erikwestberg7@gmail.com>
2024-03-21 18:28:49 +0100

    Added IParameterRepresentation

---
+++ src/Attribinter.Parameters.Core/IParameterRepresentation.cs
@@ -0,0 +1,11 @@
+﻿namespace Attribinter.Parameters;
+
+/// <summary>Represents a representation of some parameter.</summary>
+/// <typeparam name="TParameter">The type of the represented parameter.</typeparam>
+public interface IParameterRepresentation<in TParameter>
+{
+    /// <summary>Determines whether the provided parameter is the represented parameter.</summary>
+    /// <param name="parameter">The parameter.</param>
+    /// <returns>A <see cref="bool"/> indicating whether the provided parameter is the represented parameter.</returns>
+    public abstract bool Represents(TParameter parameter);
+}
